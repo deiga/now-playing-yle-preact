913b18a0db7e8b5270cbf016a21db1d61090b281
Timo Sand <timo.sand@iki.fi>
2017-11-18 11:01:37 +0200

    Adds cahing of external API calls

--- public/sw.js
+++ public/sw.js
@@ -11,11 +11,24 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  var isYleAPICall = event.request.url.match(/external.api.yle.fi\/v1/) !== null
   event.respondWith(
     caches
       .match(event.request)
       .then(function(response) {
-        return response || fetch(event.request);
+        if (response == null) console.log("cache miss", isYleAPICall, event.request.url)
+        return response || fetch(event.request).then(function(resp2) {
+                // we need to add response to the cache manually, so let's open
+                // a new cache and put responses there (for easier expiration
+                // when logging out)
+                var r = resp2.clone()
+                if (isYleAPICall) {
+                    caches.open("yle-api").then(function(cache) {
+                      cache.put(event.request, resp2.clone())
+                    })
+                }
+                return r
+            })
       })
       .catch(function(e) {
         console.log('error in service worker while intercepting fetch', e);
